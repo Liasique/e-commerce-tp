15b70555b0e51735ee1eec76a05a9a4e5abf7fbd
Ollie <olgalukianets7@gmail.com>
2024-07-30 15:40:32 +0200

    feat:Table

---
+++ e-commerce/src/commun/Table.js
@@ -0,0 +1,30 @@
+// import Table from "react-bootstrap/Table";
+
+// function Table(nom, description, prix, image, tail, id) {
+//   return (
+//     <Table striped bordered hover>
+//       <thead>
+//         <tr>
+//           <th>#</th>
+//           <th>Title</th>
+//           <th>Description</th>
+//           <th>image</th>
+//           <th>Prix</th>
+//           <th>Taille</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         <tr>
+//           <td>{id}</td>
+//           <td>{nom}</td>
+//           <td>{description}</td>
+//           <td>{image}</td>
+//           <td>{prix}</td>
+//           <td>{tail}</td>
+//         </tr>
+//       </tbody>
+//     </Table>
+//   );
+// }
+
+// export default Table;
